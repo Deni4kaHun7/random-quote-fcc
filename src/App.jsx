07e7fa343d8d4b0762ae15fd9b31c2fb14bbd768
Deni4kaHun7 <denis.serkes@gmail.com>
2024-04-23 13:04:01 +0200

    feature finished
    random quote shows on the initial render and after user clicks the button

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,33 @@
-import { useState, useEffect } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
-fetch("https://type.fit/api/quotes")
-    .then(res => res.json())
-    .then(data => console.log(data))
-function App() {
-  useEffect(() => {
-    
-  })
-  
 
+export default function App() {
+  const [allQuotes, setAllQuotes] = useState([])
+  const [quote, setQuote] = useState({});
+  let randomIndex = 0;
+
+  useEffect(() =>{
+    fetch("https://type.fit/api/quotes")
+    .then(res => res.json())
+    .then(data => {
+      setAllQuotes(data);
+      setQuote(data[Math.floor(Math.random()*data.length)])
+    })
+  }, [])
+
+  function changeQuote(){
+    randomIndex = Math.floor(Math.random()*allQuotes.length);
+    setQuote(allQuotes[randomIndex])
+  }
   return (
     <div id="quote-box">
-      <h1 id='text'>Every strike brings me closer to the next home run</h1>
-      <h2 id="author">- W.Clement Stone</h2>
-      <button id="new-quote">New quote</button>
-      <a id="tweet-quote" href=""></a>
+      <h1 id='text'>{quote.text}</h1>
+      <h2 id="author">- {quote.author}</h2>
+      <button onClick={changeQuote} id="new-quote">New quote</button>
+      <a id="tweet-quote" href="twitter.com/intent/tweet"></a>
     </div>
   )
 }
 
-export default App
+
+
